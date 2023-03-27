60fdc951afc90df90763aa8e7eba5550459cc435
DECHENAUD RAPHAEL p2106803 <raphael.dechenaud@etu.univ-lyon1.fr>
2023-03-27 19:42:54 +0200

    zoom sur la position du joueurs

--- app/src/main/java/com/example/tp_6/MapsActivity.java
+++ app/src/main/java/com/example/tp_6/MapsActivity.java
@@ -1,10 +1,14 @@
 package com.example.tp_6;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -18,6 +22,14 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.tp_6.databinding.ActivityMapsBinding;
 import com.google.maps.android.clustering.ClusterManager;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 
 import java.util.ArrayList;
 import java.util.Objects;
@@ -27,6 +39,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
     private ArrayList<Antenne> antennes = new ArrayList<>();
+    private static final int MY_PERMISSIONS_REQUEST_LOCATION = 10;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,10 +55,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         Objects.requireNonNull(mapFragment).getMapAsync(this);
     }
 
-    @SuppressLint("PotentialBehaviorOverride")
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        setUpMap();
         ClusterManager<ClusterItemClass> clusterManager = new ClusterManager<>(this, mMap);
 
         for (Antenne antenne : antennes) {
@@ -56,14 +70,41 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.setOnCameraIdleListener(clusterManager);
         mMap.setOnMarkerClickListener(clusterManager);
 
-        LatLng paris = new LatLng(48.8566, 2.3522); // coordonnées de Paris
+        /*LatLng paris = new LatLng(48.8566, 2.3522); // coordonnées de Paris
         float zoomLevel = 10; // zoom de départ
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(paris, zoomLevel));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(paris, zoomLevel)); */
 
         MyClusterRenderer renderer = new MyClusterRenderer(this, mMap, clusterManager);
         clusterManager.setRenderer(renderer);
     }
 
+    private void setUpMap() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true); // Activer le bouton de localisation sur la carte
+            mMap.getUiSettings().setMyLocationButtonEnabled(true); // Activer la localisation
+            mMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
+                @Override
+                public void onMyLocationChange(Location location) {
+                    // Récupérer la position actuelle de l'utilisateur
+                    LatLng myLocation = new LatLng(location.getLatitude(), location.getLongitude());
+
+                    // Ajouter un marqueur à la position de l'utilisateur
+                    mMap.addMarker(new MarkerOptions().position(myLocation).title("Ma position"));
+
+                    // Zoomer sur la position de l'utilisateur
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 10));
+                }
+            });
+        } else {
+            // Demander la permission de localisation à l'utilisateur
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.ACCESS_COARSE_LOCATION }, MY_PERMISSIONS_REQUEST_LOCATION);
+
+            // Déplacer la carte sur Paris
+            LatLng paris = new LatLng(48.8534, 2.3488);
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(paris, 10));
+        }
+    }
 
 
 
